b9e277104d2b545d1126c91a58d1e1c984a76d17
elfsvet <stepanmatysik@gmail.com>
2022-07-20 00:02:02 -0400

    added particles from a template.

--- src/App.js
+++ src/App.js
@@ -1,73 +1,18 @@
 
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar'
-import Particles from 'react-tsparticles';
-
+import Particles from "./Particles";
+import Header from './screens/header/Header';
 
 
 function App() {
 
+
   return (
     <div className="App">
       <Navigation />
-      <Particles 
-        id="tsparticles"
-        options={{
-          background: {
-            color: {
-              value: "#212529",
-            },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              // onHover: {
-              //   enable: true,
-              //   mode: "repulse",
-              // },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 200,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#8390fa",
-            },
-            links: {
-              color: "#ffff5f",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 1,
-              straight: false,
-            },
-          },
-        }}
-      />
+      <Header />
+      <Particles />
     </div>
   );
 }
