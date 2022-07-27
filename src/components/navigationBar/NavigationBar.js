50b698ae2e6812eb19ada0ebbd216b198e6b6ea1
elfsvet <stepanmatysik@gmail.com>
2022-07-27 13:09:01 -0400

    fixed zindex in nav

--- src/components/navigationBar/NavigationBar.js
+++ src/components/navigationBar/NavigationBar.js
@@ -12,7 +12,7 @@ const  createLinks = () => {
 const NavigationBar = () => {
     return (
         <div id='home'>
-            <Navbar className='navigation-container'   collapseOnSelect expand="md">
+            <Navbar className='navigation-container'   collapseOnSelect expand="md" style={{ zIndex: 2}}>
                 <Navbar.Brand href="#home" style={{marginLeft: '1rem'}}>Stepan Matysik</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse style={{justifyContent:"flex-end", marginRight: "1rem", borderColor: "none"}}>
